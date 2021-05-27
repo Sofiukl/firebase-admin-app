ffed2248645ca60b2039c650c63aca35b335482d
Sofikul Mallick <sofikul.projects@gmail.com>
2021-05-28 00:59:42 +0530

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ async function startServer() {
   routes({ app })
 
   app.use(express.static(path.join(__dirname, "..", "build/static")));
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
 
   app.listen(process.env.PORT || 3101, () => {
     // eslint-disable-next-line no-console
